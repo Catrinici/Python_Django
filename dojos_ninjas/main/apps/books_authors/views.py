df8748b5e076f5121e83ff127259f169634a030b
Catrinici <catrinici.antonina@gmail.com>
2019-09-13 14:33:37 -0700

    first commit

---
+++ dojos_ninjas/main/apps/books_authors/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+
+    return render(request, "dojos_ninjas/index.html")
--- dojos_ninjas/main/main/settings.py
+++ dojos_ninjas/main/main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.dojos_ninjas',
+    'apps.books_authors',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

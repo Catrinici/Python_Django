8f9cf4f848a73ddd160f8bd19975f87232ff1ca2
Catrinici <catrinici.antonina@gmail.com>
2019-09-14 15:41:48 -0700

    first commit

---
+++ pollster_project/pollster/polls/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Question, Choice
+
+# Get questions and display them
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+#Show specific questions and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    return render(request, 'polls/results.html', { 'question': question})
